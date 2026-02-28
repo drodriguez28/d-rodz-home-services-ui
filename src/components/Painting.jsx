b553b2c362a5fb9df808d1d8b4fc623281c53c10
DXR4KMK_thdgit <deivid_rodriguez1@homedepot.com>
2026-02-28 12:35:42 -0500

    added correct image

--- src/components/Painting.jsx
+++ src/components/Painting.jsx
@@ -13,8 +13,7 @@ export const Painting = () => {
     "/images/paint-2.jpeg",
     "/images/paint-3.jpeg",
     "/images/paint-4.jpeg",
-    "/images/videos/paint-vid-1.mp4",
-    // "/images/videos/paint-vid-2.mp4",
+    "/images/videos/paint-vid-2.mp4",
     "/images/videos/paint-vid-3.mp4",
   ];
 
